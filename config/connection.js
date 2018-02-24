e4c3bb631fbe5a12e1d2d4650138e849f7e9f038
katkeivens <katkeivens@gmail.com>
2018-02-24 00:08:44 -0800

    updated credentials

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Fritz123",
+  password: "******",
   database: "burgers_db"
 });
 
